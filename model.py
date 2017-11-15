f6c40afc02c39b95460096ad2e6d21de2361f1ac
leftthomas <leftthomas@qq.com>
2017-11-15 22:52:52 +0800

    add initialize weights

--- model.py
+++ model.py
@@ -12,12 +12,20 @@ class Net(nn.Module):
         self.conv4 = nn.Conv2d(32, 3 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
 
+        self._initialize_weights()
+
     def forward(self, x):
         x = self.relu(self.conv1(x))
         x = self.relu(self.conv2(x))
         x = self.relu(self.conv3(x))
         x = self.relu(self.pixel_shuffle(self.conv4(x)))
         return x
+
+    def _initialize_weights(self):
+        nn.init.orthogonal(self.conv1.weight, nn.init.calculate_gain('relu'))
+        nn.init.orthogonal(self.conv2.weight, nn.init.calculate_gain('relu'))
+        nn.init.orthogonal(self.conv3.weight, nn.init.calculate_gain('relu'))
+        nn.init.orthogonal(self.conv4.weight)
 
 
 if __name__ == "__main__":
